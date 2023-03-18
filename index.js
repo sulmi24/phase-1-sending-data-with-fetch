74dc53b812034b531e2b8e1a3aeab871c9061fd3
sulmi24 <a.sulmi24@gmail.com>
2023-03-18 21:41:20 +0200

    Solved the assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Add your code here
+let forminput = document.querySelector("form");
+
+forminput.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let name = document.getElementById("userName").value;
+  let email = document.getElementById("userMail").value;
+  submitData(name, email);
+});
+
+function submitData(name, email) {
+  console.log(name);
+  console.log(email);
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      let heading = document.createElement("h3");
+      heading.textContent = data["id"];
+      forminput.appendChild(heading);
+    })
+    .catch(function (error) {
+      let p = document.createElement("p");
+      p.textContent = error.message;
+      forminput.appendChild(p);
+    });
+}
